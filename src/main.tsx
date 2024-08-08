a87b9a6691742792d097b7f401ced18a9b41acd1
Maxwell Jones <maxxjonesyy@gmail.com>
2024-08-08 11:32:12 +1000

    frontend: setup axios base url

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
