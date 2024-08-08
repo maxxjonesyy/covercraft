1d6fc4b5b7d90210a2653337785cdc470070e02b
Maxwell Jones <maxxjonesyy@gmail.com>
2024-08-08 11:32:34 +1000

    backend: setup cors for development env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,29 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  process.env.LOCAL_ENV1,
+  process.env.LOCAL_ENV2,
+
+  //   Production URL will go here
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.json({ limit: "1mb" }));
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
