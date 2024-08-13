457a950111ab8567503623ca7b72cb8e54f00b1e
Maxwell Jones <maxxjonesyy@gmail.com>
2024-08-13 18:35:32 +1000

    frontend: add toast notification library

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import axios from "axios";
+import { Toaster } from "react-hot-toast";
 
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster />
     <App />
   </React.StrictMode>
 );
