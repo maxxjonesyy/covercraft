291f1f21eedf9c9763cb796c3f4579c4c9b5d7f0
Maxwell Jones <maxxjonesyy@gmail.com>
2024-08-21 16:28:25 +1000

    frontend: setup react-router for views

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import CoverletterCreate from "./views/CoverLetterCreate";
+import CoverLetterReady from "./views/CoverLetterReady";
 
 function App() {
   return (
-    <div className='min-h-screen bg-gradient-to-b from-stone-200 to-zinc-200'>
+    <div className="min-h-screen px-5 bg-gradient-to-b from-stone-100 to-zinc-100">
       <Navbar />
+
+      <Routes>
+        <Route path="/coverletter" element={<CoverletterCreate />} />
+        <Route path="/coverletter/result" element={<CoverLetterReady />} />
+      </Routes>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_BASE_URL;
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
