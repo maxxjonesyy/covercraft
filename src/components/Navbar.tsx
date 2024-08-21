15371a52b9c4d81067df47958dc608c6fd606c67
Maxwell Jones <maxxjonesyy@gmail.com>
2024-08-21 16:29:02 +1000

    frontend: refactor navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 function Navbar() {
   return (
-    <nav className='container flex items-center justify-between mx-auto p-5 bg-white/20 rounded-md shadow-sm'>
-      <h1 className='text-xl'>Covercraft</h1>
+    <nav className="max-w-[75rem] mx-auto flex items-center justify-between mx-auto p-5 bg-white/20 rounded-md shadow-sm">
+      <h1 className="text-lg md:text-xl">Covercraft</h1>
 
-      <ul className='flex items-center gap-5 text-sm'>
+      <ul className="flex items-center gap-5 text-sm">
         <li>
-          <button className='px-4 py-2 border border-gray-300 rounded shadow-sm'>
+          <button className="px-4 py-2 border border-secondary rounded">
             Sign in / Sign up
           </button>
         </li>
