a8940f597f5e4ffd108b8c5091fcfce1a2f1d153
Maxwell Jones <maxxjonesyy@gmail.com>
2024-08-22 10:48:17 +1000

    frontend: refactor navbar styles

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import ButtonLink from "./ButtonLink";
+
+import loginSVG from "../../assets/icons/login.svg";
+
+function Navbar() {
+  return (
+    <nav className="max-w-[75rem] mx-auto flex items-center justify-between mx-auto p-5 bg-white/20 rounded-md shadow-sm">
+      <Link to="/">
+        <h1 className="text-xl md:text-2xl">Covercraft</h1>
+      </Link>
+
+      <ul className="flex items-center gap-3 text-sm">
+        <li>
+          <ButtonLink text="Login" pathname="/login" />
+        </li>
+        <li>
+          <ButtonLink
+            text="Register"
+            pathname="/register"
+            image={{ url: loginSVG, alt: "click to login" }}
+          />
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
