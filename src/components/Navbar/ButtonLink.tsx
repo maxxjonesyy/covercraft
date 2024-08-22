105242eb3e589efdd596b72a7ceef044c6db4666
Maxwell Jones <maxxjonesyy@gmail.com>
2024-08-22 10:48:36 +1000

    frontend: create component for button navlinks

---
+++ src/components/Navbar/ButtonLink.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+interface Props {
+  text: string;
+  pathname: string;
+  image?: {
+    url: string;
+    alt: string;
+  };
+}
+
+function ButtonLink({ text, pathname, image }: Props) {
+  return (
+    <Link to={pathname}>
+      <button className="flex items-center gap-1 px-4 py-1 border border-secondary rounded transition-color duration-300 hover:bg-gray-200/50">
+        {text}
+        {image && <img src={image.url} alt={image.alt} />}
+      </button>
+    </Link>
+  );
+}
+
+export default ButtonLink;
