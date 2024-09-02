5ecd2983d60955d2212bd5fc3d78eb2f1de8d428
Maxwell Jones <maxxjonesyy@gmail.com>
2024-09-02 16:39:02 +1000

    frontend: add framer animation to coverletter view

--- src/views/CoverLetterCreate.tsx
+++ src/views/CoverLetterCreate.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import ResizableTextArea from "../components/ResizableTextArea";
 import extractFileText from "../utils/extractFileText";
@@ -47,74 +48,78 @@ function CoverletterCreate() {
   }, [coverLetter]);
 
   return (
-    <div className="container">
-      <h1 className="text-3xl mb-2">Create your cover letter</h1>
-      <p>Upload your resume and job details to get started</p>
+    <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0 }}>
+      <div className="container">
+        <h1 className="text-3xl mb-2">Create your cover letter</h1>
+        <p>Upload your resume and job details to get started</p>
 
-      <form
-        className={`w-full mt-10 ${fetchCoverLetter.isPending && "opacity-60"}`}
-        onSubmit={(e: FormEvent) => {
-          e.preventDefault();
-          if (validateForm()) fetchCoverLetter.mutate(formData);
-        }}>
-        <label htmlFor="resume">Upload resume:</label>
-        <input
-          type="file"
-          name="resume"
-          id="resume"
-          accept=".pdf"
-          className="max-w-3xl block shadow-sm rounded text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none file:bg-accentBlue file:text-white file:rounded file:border-0 file:me-4 file:py-1.5 file:px-5 file:cursor-pointer"
-          onChange={async (e: ChangeEvent<HTMLInputElement>) => {
-            const pdfText = await extractFileText(e.target.files?.[0]);
+        <form
+          className={`w-full mt-10 ${
+            fetchCoverLetter.isPending && "opacity-60"
+          }`}
+          onSubmit={(e: FormEvent) => {
+            e.preventDefault();
+            if (validateForm()) fetchCoverLetter.mutate(formData);
+          }}>
+          <label htmlFor="resume">Upload resume:</label>
+          <input
+            type="file"
+            name="resume"
+            id="resume"
+            accept=".pdf"
+            className="max-w-3xl block shadow-sm rounded text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none file:bg-accentBlue file:text-white file:rounded file:border-0 file:me-4 file:py-1.5 file:px-5 file:cursor-pointer"
+            onChange={async (e: ChangeEvent<HTMLInputElement>) => {
+              const pdfText = await extractFileText(e.target.files?.[0]);
 
-            if (pdfText) {
-              setFormData({ ...formData, resume: pdfText });
-            }
-          }}
-        />
+              if (pdfText) {
+                setFormData({ ...formData, resume: pdfText });
+              }
+            }}
+          />
 
-        <label htmlFor="title">Job title:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={formData.title}
-          onChange={handleChange}
-          className="max-w-3xl"
-        />
+          <label htmlFor="title">Job title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="max-w-3xl"
+          />
 
-        <label htmlFor="company">Job company:</label>
-        <input
-          type="text"
-          id="company"
-          name="company"
-          value={formData.company}
-          onChange={handleChange}
-          className="max-w-3xl"
-        />
+          <label htmlFor="company">Job company:</label>
+          <input
+            type="text"
+            id="company"
+            name="company"
+            value={formData.company}
+            onChange={handleChange}
+            className="max-w-3xl"
+          />
 
-        <label htmlFor="description">Job decription:</label>
-        <ResizableTextArea
-          id="description"
-          name="description"
-          className="text-sm"
-          value={formData.description}
-          onChange={handleChange}
-        />
+          <label htmlFor="description">Job decription:</label>
+          <ResizableTextArea
+            id="description"
+            name="description"
+            className="text-sm"
+            value={formData.description}
+            onChange={handleChange}
+          />
 
-        <button
-          type="submit"
-          disabled={fetchCoverLetter.isPending}
-          className="flex items-center justify-center gap-2 mt-3 bg-accentBlue text-white px-4 py-2 rounded shadow transition-all hover:scale-105">
-          {!fetchCoverLetter.isPending ? (
-            <img src={createSVG} alt="create cover letter" />
-          ) : (
-            <Loader />
-          )}
-          Create cover letter
-        </button>
-      </form>
-    </div>
+          <button
+            type="submit"
+            disabled={fetchCoverLetter.isPending}
+            className="flex items-center justify-center gap-2 mt-3 bg-accentBlue text-white px-4 py-2 rounded shadow transition-all hover:scale-105">
+            {!fetchCoverLetter.isPending ? (
+              <img src={createSVG} alt="create cover letter" />
+            ) : (
+              <Loader />
+            )}
+            Create cover letter
+          </button>
+        </form>
+      </div>
+    </motion.div>
   );
 }
 
--- src/views/CoverLetterReady.tsx
+++ src/views/CoverLetterReady.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import ResizableTextArea from "../components/ResizableTextArea";
 import Button from "../components/Button";
 import toast from "react-hot-toast";
+import { motion } from "framer-motion";
 
 import backArrowSVG from "../assets/icons/back-arrow.svg";
 import clickToCopySVG from "../assets/icons/click-to-copy.svg";
@@ -28,55 +29,57 @@ function CoverLetterReady() {
   }, []);
 
   return (
-    <div className="container">
-      <h1 className="text-3xl">
-        {formData.title} at {formData.company}
-      </h1>
-      <p className="mt-3">
-        Click to copy or save to your collection - it's that simple!
-      </p>
+    <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0 }}>
+      <div className="container">
+        <h1 className="text-3xl">
+          {formData.title} at {formData.company}
+        </h1>
+        <p className="mt-3">
+          Click to copy or save to your collection - it's that simple!
+        </p>
 
-      <div className="flex items-center justify-between max-w-3xl  mt-10">
-        <Button
-          text="Back to form"
-          image={{ url: backArrowSVG, alt: "back to form" }}
-          onClick={() => navigate("/coverletter")}
-          isBlue
-        />
-
-        <Button
-          text="Save"
-          image={{ url: saveSVG, alt: "save cover letter" }}
-        />
-      </div>
-
-      <div
-        className="relative mt-5 overflow-hidden"
-        onMouseEnter={animateClickToCopy}
-        onMouseLeave={animateClickToCopy}
-        onClick={() => {
-          navigator.clipboard.writeText(coverLetter);
-          toast.success("Successfully copied!");
-        }}>
-        <div className="absolute h-[99%] z-10 w-full transition-all hover:cursor-pointer hover:bg-accentBlue/5" />
-
-        <div
-          ref={clickToCopyRef}
-          className="absolute transition-all duration-300 top-0 right-[-50%] p-5">
+        <div className="flex items-center justify-between max-w-3xl  mt-10">
           <Button
-            text="Click to copy"
-            image={{ url: clickToCopySVG, alt: "click to copy cover letter" }}
+            text="Back to form"
+            image={{ url: backArrowSVG, alt: "back to form" }}
+            onClick={() => navigate("/coverletter")}
             isBlue
+          />
+
+          <Button
+            text="Save"
+            image={{ url: saveSVG, alt: "save cover letter" }}
           />
         </div>
 
-        <ResizableTextArea
-          readOnly
-          value={coverLetter}
-          className="text-sm hover:cursor-pointer"
-        />
+        <div
+          className="relative mt-5 overflow-hidden"
+          onMouseEnter={animateClickToCopy}
+          onMouseLeave={animateClickToCopy}
+          onClick={() => {
+            navigator.clipboard.writeText(coverLetter);
+            toast.success("Successfully copied!");
+          }}>
+          <div className="absolute h-[99%] z-10 w-full transition-all hover:cursor-pointer hover:bg-accentBlue/5" />
+
+          <div
+            ref={clickToCopyRef}
+            className="absolute transition-all duration-300 top-0 right-[-50%] p-5">
+            <Button
+              text="Click to copy"
+              image={{ url: clickToCopySVG, alt: "click to copy cover letter" }}
+              isBlue
+            />
+          </div>
+
+          <ResizableTextArea
+            readOnly
+            value={coverLetter}
+            className="text-sm hover:cursor-pointer"
+          />
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
