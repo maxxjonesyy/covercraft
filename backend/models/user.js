48c5e7f73a3cd7d9d29e6e9dedc57e395e01eaf8
Maxwell Jones <maxxjonesyy@gmail.com>
2024-10-12 14:37:31 +1100

    backend: update user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
+const coverLetterSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  company: { type: String, required: true },
+  coverletter: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  tokenCount: { type: Number, default: 0 },
+  totalCoverLetters: { type: Number, default: 0 },
+  savedCoverLetters: [coverLetterSchema],
   createdAt: { type: Date, default: Date.now },
 });
 
