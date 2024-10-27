0695a45e168a555d7f972fd6dfb0e770dbb56ece
Max Jones <maxxjonesyy@gmail.com>
2024-10-27 15:19:03 +1100

    frontend: add colour to Loader component params

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,15 @@
 import ClipLoader from "react-spinners/ClipLoader";
 
-function Loader({ size = 20 }: { size?: number }) {
+function Loader({
+  size = 20,
+  colour = "white",
+}: {
+  size?: number;
+  colour?: string;
+}) {
   return (
     <span className="flex items-center">
-      <ClipLoader color="#FFFFFF" size={size} speedMultiplier={0.75} />
+      <ClipLoader size={size} color={colour} speedMultiplier={0.75} />
     </span>
   );
 }
