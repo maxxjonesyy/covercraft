14104abed68815d5f47fcbfcb2646e4c1c64027a
Max Jones <maxxjonesyy@gmail.com>
2024-10-29 22:58:30 +1100

    backend: functionality to unsave cover letter

--- backend/controllers/CoverletterController.js
+++ backend/controllers/CoverletterController.js
@@ -14,6 +14,34 @@ async function getCoverLetters(req, res) {
   }
 }
 
+async function deleteCoverLetter(req, res) {
+  const { id } = req.params;
+  const { email } = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: "Error deleting, no id provided" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ error: "User not found" });
+  }
+
+  try {
+    user.savedCoverLetters = user.savedCoverLetters.filter(
+      (letter) => letter.id !== id
+    );
+    await user.save();
+    res.status(200).json({ data: "Cover letter deleted" });
+  } catch (error) {
+    console.error("Error deleting cover letter:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting cover letter" });
+  }
+}
+
 async function saveCoverLetter(req, res) {
   const { title, company, coverLetter, email } = req.body;
 
@@ -50,5 +78,6 @@ async function saveCoverLetter(req, res) {
 
 module.exports = {
   getCoverLetters,
+  deleteCoverLetter,
   saveCoverLetter,
 };
--- backend/routes/coverletter-routes.js
+++ backend/routes/coverletter-routes.js
@@ -4,10 +4,12 @@ const { authenticateToken } = require("../middleware/middleware");
 
 const {
   getCoverLetters,
+  deleteCoverLetter,
   saveCoverLetter,
 } = require("../controllers/CoverletterController");
 
 router.get("/savedCoverLetters", authenticateToken, getCoverLetters);
 router.post("/savedCoverLetters", authenticateToken, saveCoverLetter);
+router.delete("/savedCoverLetters/:id", authenticateToken, deleteCoverLetter);
 
 module.exports = router;
