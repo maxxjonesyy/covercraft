4ac78d7f4916ab5355919f79199c23f61d4ca7e4
Max Jones <maxxjonesyy@gmail.com>
2024-11-17 13:45:28 +1100

    add pricing tier cards to token page

--- src/views/Token.tsx
+++ src/views/Token.tsx
@@ -1,12 +1,32 @@
 import { motion } from "framer-motion";
+import { FeaturedPriceCard, PriceCard } from "../components";
 
 function Token() {
   return (
-    <motion.div
-      animate={{ y: 0, opacity: 1 }}
-      initial={{ y: 300, opacity: 0 }}
-      className="container">
-      <h1>Token</h1>
+    <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0 }}>
+      <div className="container">
+        <h1 className="text-3xl mb-2">Add Tokens</h1>
+      </div>
+
+      <section className="flex justify-center flex-wrap gap-3">
+        <PriceCard tokenCount={5} price={5} pricePerToken={1} paymentUrl="" />
+
+        <FeaturedPriceCard
+          tokenCount={15}
+          price={10}
+          pricePerToken={0.67}
+          bonusTokens={5}
+          paymentUrl=""
+        />
+
+        <PriceCard
+          tokenCount={25}
+          price={15}
+          pricePerToken={0.6}
+          bonusTokens={10}
+          paymentUrl=""
+        />
+      </section>
     </motion.div>
   );
 }
