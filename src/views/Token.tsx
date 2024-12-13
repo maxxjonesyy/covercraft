606cc0aa194af1a0fbecf72fd26bf2252b940462
Max Jones <maxxjonesyy@gmail.com>
2024-12-13 17:09:21 +1100

    frontend: update PriceCards component on token page

--- src/views/Token.tsx
+++ src/views/Token.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FeaturedPriceCard, PriceCard } from "../components";
+import { PriceCards } from "../components";
 import { UserContext } from "../context/UserContext";
 import { useContext } from "react";
 import { useEffect } from "react";
@@ -60,39 +60,18 @@ function Token() {
 
   return (
     <motion.div animate={{ y: 0, opacity: 1 }} initial={{ y: 300, opacity: 0 }}>
-      <div className="container pb-[3rem] flex flex-col items-center">
+      <div className="container pb-[6rem] flex flex-col items-center">
         <img src={StripeBadge} alt="Stripe" className="h-8 w-auto mb-10" />
         <h1 className="text-3xl mb-2">Add Tokens</h1>
 
-        <p className="text-gray-600">
+        <p className="text-lg text-center text-gray-600">
           Use tokens to generate high-quality, tailored cover letters.
         </p>
       </div>
 
-      <section className="flex flex-col md:flex-row justify-center gap-6 px-4 md:px-8 lg:px-12 py-10">
-        <PriceCard
-          tokenCount={5}
-          price={5}
-          pricePerToken={1}
-          priceId="price_1QLfUtF40ixAgrNfyuMWGKUh"
-        />
-
-        <FeaturedPriceCard
-          tokenCount={15}
-          price={10}
-          pricePerToken={0.67}
-          bonusTokens={5}
-          priceId="price_1QLfe2F40ixAgrNfx1zpyWKP"
-        />
-
-        <PriceCard
-          tokenCount={25}
-          price={15}
-          pricePerToken={0.6}
-          bonusTokens={10}
-          priceId="price_1QLffZF40ixAgrNfYC0QcWZf"
-        />
-      </section>
+      <div className="pb-[6rem]">
+        <PriceCards />
+      </div>
     </motion.div>
   );
 }
