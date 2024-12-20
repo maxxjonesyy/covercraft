95d94c5d7f4eeec1bd0b6c223e0f0a4d383a5896
Max Jones <maxxjonesyy@gmail.com>
2024-12-20 15:01:05 +1100

    backend: update allowedOrigins

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://covercraft-mj.netlify.app",
+  "https://covercraft-mj.netlify.app/",
 ];
 
 const corsOptions = {
