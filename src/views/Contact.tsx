3e204819f7f61beefbf203ad05d8984aa52eb28c
Max Jones <maxxjonesyy@gmail.com>
2025-01-31 19:45:45 +1100

    frontend: update 404 page responsiveness

---
+++ src/views/Contact.tsx
@@ -0,0 +1,7 @@
+import { motion } from "framer-motion";
+
+function Contact() {
+  return <motion.form></motion.form>;
+}
+
+export default Contact;
--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -5,9 +5,11 @@ function NotFound() {
     <motion.div
       animate={{ y: 0, opacity: 1 }}
       initial={{ y: 300, opacity: 0 }}
-      className="container flex flex-col items-center justify-center">
-      <h1 className="text-3xl mb-3">404 - Page not found</h1>
-      <p>Sorry, the page you're looking for does not exist</p>
+      className="container flex flex-col items-center justify-center text-center">
+      <h1 className="text-2xl md:text-3xl mb-3">404 - Page not found</h1>
+      <p className="text-sm md:text-base">
+        Sorry, the page you're looking for does not exist
+      </p>
     </motion.div>
   );
 }
