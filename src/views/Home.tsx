1cfb2789f403cbaba8a719be21bf4e4b78d5c10b
Max Jones <maxxjonesyy@gmail.com>
2026-01-07 09:46:18 -0800

    update hero section ui

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -14,24 +14,30 @@ function Home() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
           <div className="grid md:grid-cols-2 items-center gap-16">
             <div className="space-y-6 text-left">
-              <h1 className="bg-accentBlue/10 inline-block px-4 py-2 rounded-full text-[#3943B7] border border-[#3943B7]/30 text-sm tracking-wide">
-                Sign up and get a free cover letter on us!
+              <h1 className="bg-accentBlue/10 inline-block px-4 py-2 rounded-full text-[#3943B7] border border-[#3943B7]/30 text-xs tracking-wide">
+                Sign up, get a free one on us! <br />
               </h1>
-              <h1 className="text-4xl font-extrabold text-gray-900 leading-tight">
-                Craft{" "}
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#3943B7] to-purple-600">
-                  Tailored Cover Letters
+              <h1 className="text-4xl md:text-5xl font-extrabold text-gray-900 leading-tight">
+                <span className="relative inline-block px-4 py-1">
+                  <span className="absolute inset-0 bg-gradient-to-r from-[#3943B7] via-purple-500 to-purple-600 transform skew-y-4 scale-105 rounded-2xl blur-sm opacity-50 -z-10"></span>
+                  <span className="absolute inset-0 bg-gradient-to-r from-[#3943B7] to-purple-600 transform -skew-x-6 rounded-lg -z-10"></span>
+                  <span className="relative text-white font-bold">
+                    Tailored Cover Letters.
+                  </span>
                 </span>{" "}
-                in Seconds
+                <br />
+                In Seconds
               </h1>
               <p className="text-lg text-gray-600 leading-relaxed">
-                Leverage us, to create tailored cover letters that perfectly
-                match your job description and experience.
+                Stop spending hours on every application. Get personalised,
+                professional cover letters instantly.
               </p>
 
               <div className="w-xl">
                 <Link to="/coverletter">
-                  <button className="flex items-center bg-gradient-to-r from-[#3943B7] to-purple-600 text-white px-8 py-3 rounded-lg shadow-xl hover:shadow-2xl transition transform hover:-translate-y-1 group">
+                  <button className="flex items-center  text-white px-6 py-2 rounded-lg shadow-xl hover:shadow-2xl transition transform hover:-translate-y-1 group">
+                    <span className="absolute inset-0 bg-gradient-to-r from-[#3943B7] via-purple-500 to-purple-600 transform skew-y-4 scale-105 rounded-2xl blur-sm opacity-50 -z-10"></span>
+                    <span className="absolute inset-0 bg-gradient-to-r from-[#3943B7] to-purple-600 transform -skew-x-6 rounded-lg -z-10"></span>
                     <Zap className="mr-2 group-hover:animate-pulse" />
                     Create Cover Letter
                   </button>
@@ -40,7 +46,6 @@ function Home() {
             </div>
 
             <div className="relative pt-16 mt-15 md:mt-0 md:block">
-              <h2 className="text-3xl font-bold">How does it work?</h2>
               <div className="bg-white/80 rounded-2xl shadow-2xl p-6 mt-3 transform -rotate-3 hover:rotate-0 transition-transform">
                 <div className="space-y-3">
                   <div className="bg-gradient-to-r from-accentBlue/10 to-white rounded-lg p-3 flex items-center">
